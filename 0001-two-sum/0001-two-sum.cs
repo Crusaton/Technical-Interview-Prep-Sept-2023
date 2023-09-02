c988db0845a142d585262abfa438f67aec905872
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-01 23:02:40 -0400

    Time: 131 ms (92.80%), Space: 45 MB (15.68%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        
+        Dictionary<int, int> seenNums = new();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int remainder = target - nums[i];
+            if(seenNums.ContainsKey(remainder))
+            {
+                return new int[] { seenNums[remainder], i };
+            }
+            else
+            {
+                seenNums.TryAdd(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
