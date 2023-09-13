0d7d2a622893f1e7d732ce5dda5e7a9c1d1229d6
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-12 21:04:29 -0400

    Time: 70 ms (56.55%), Space: 37.8 MB (77.36%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> stack = new();
+        
+        foreach(char c in s)
+        {
+            if(c == '(')
+            {
+                stack.Push(')');
+            }
+            else if(c == '[')
+            {
+                stack.Push(']');
+            }
+            else if(c == '{')
+            {
+                stack.Push('}');
+            }
+            else if(stack.Count == 0 || stack.Pop() != c)
+            {
+                return false;
+            }
+        }
+        if(stack.Count != 0)
+        {
+            return false;
+        }
+        return true;
+    }
+}
