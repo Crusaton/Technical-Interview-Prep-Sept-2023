cb6242881b8d05fafed41b159ab60aa1b6d6cf1e
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-18 18:04:42 -0400

    Time: 71 ms (67.07%), Space: 39.9 MB (65.54%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        
+        int[] seenChars = new int[26];
+        
+        foreach(char c in s)
+        {
+            seenChars[c - 'a']++;
+        }
+        foreach(char c in t)
+        {
+            seenChars[c - 'a']--;
+        }
+        foreach(int i in seenChars)
+        {
+            if(i != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
