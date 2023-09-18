ece7518f2db00d1a21f3c3ac7fc5c75f67a61098
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-18 18:22:00 -0400

    Time: 84 ms (43.53%), Space: 38.6 MB (57.32%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cs
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cs
@@ -13,16 +13,27 @@
  */
 public class Solution {
     public TreeNode InvertTree(TreeNode root) {
+        
+        // If root is null it means we've reached the end of our tree / recursive loop.
         if(root == null)
         {
             return null;
         }
         
+        
+        // Temp value to not lose the left side value.
         var tmp = root.left;
+        
+        // Set left equal to right.
         root.left = root.right;
+        
+        // Set right equal to our temp value (left).
         root.right = tmp;
         
+        
+        // Recursively loop to flip the left side tree.
         InvertTree(root.left);
+        // Recursively loop to flip the right side tree.
         InvertTree(root.right);
         
         return root;
