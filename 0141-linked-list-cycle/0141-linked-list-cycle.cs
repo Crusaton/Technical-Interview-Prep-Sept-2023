5290f668e87f6294dc30204679f8217cc24079aa
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-19 18:41:02 -0400

    Time: 80 ms (97.29%), Space: 43.1 MB (52.96%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cs
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cs
@@ -15,10 +15,14 @@ public class Solution {
         ListNode slow = head;
         ListNode fast = head;
         
+        // If there is no cycle in the linked list eventually fast will break out of the loop. Otherwise eventually fast will be the same node as slow.
         while(fast != null && fast.next != null)
         {
+            // fast jumps forward two
             fast = fast.next.next;
+            // slow jumps forward one
             slow = slow.next;
+            // If they equal eachother we know we have a cycle.
             if(slow == fast)
             {
                 return true;
