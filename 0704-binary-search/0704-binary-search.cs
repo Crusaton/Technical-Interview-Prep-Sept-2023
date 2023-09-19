a834f3a36ae8e186ff065f0db8b3c31d31fa684c
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-19 18:23:13 -0400

    Time: 115 ms (47.99%), Space: 51 MB (91.30%) - LeetHub

--- 0704-binary-search/0704-binary-search.cs
+++ 0704-binary-search/0704-binary-search.cs
@@ -1,6 +1,7 @@
 public class Solution {
     public int Search(int[] nums, int target) {
         
+        
         int left = 0;
         int right = nums.Length - 1;
         
@@ -8,20 +9,19 @@ public class Solution {
         {
             int mid = left + ((right - left) / 2);
             
-            if(nums[mid] > target)
+            if(nums[mid] == target)
             {
-                right = mid - 1;
+                return mid;
             }
             else if(nums[mid] < target)
             {
                 left = mid + 1;
             }
-            else
+            else if(nums[mid] > target)
             {
-                return mid;
+                right = mid - 1;
             }
         }
-        
         return -1;
     }
 }
