fcddd393b8fc3843ca9e6594d912158ff43087fa
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-19 21:12:38 -0400

    Time: 92 ms (81.18%), Space: 43.8 MB (55.07%) - LeetHub

--- 0136-single-number/0136-single-number.cs
+++ 0136-single-number/0136-single-number.cs
@@ -1,6 +1,7 @@
 public class Solution {
     public int SingleNumber(int[] nums) {
         
+        // If the array only has one item we found our answer.
         if(nums.Count() == 1)
         {
             return nums[0];
@@ -8,8 +9,13 @@ public class Solution {
         
         int result = 0;
         
+        // Iterate through our array. 
         for(int i = 0; i < nums.Length; i++)
         {
+            // Performing an XOR operation.
+            // It basically turns the question into an equation doing this.
+            // If nums = [4,1,2,1,2]
+            // ((2^2)^(1^1)^(4)) = (0^0^4)
             result ^=nums[i];
         }
         return result;
