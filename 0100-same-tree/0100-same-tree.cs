d5153174f4e800ee121ed5f41e3b0bb1a0e58a99
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-23 12:52:39 -0400

    Time: 93 ms (24.85%), Space: 40.1 MB (60.14%) - LeetHub

--- 0100-same-tree/0100-same-tree.cs
+++ 0100-same-tree/0100-same-tree.cs
@@ -14,11 +14,14 @@
 public class Solution {
     public bool IsSameTree(TreeNode p, TreeNode q) {
         
+        // If we've reached the end of one of the trees we should have reached the end of both. So if they're not both null we know they aren't the same tree.
         if(p == null || q == null)
         {
             return p == q;
         }
         
+        
+        // Check our current values then continue checking the rest of the trees.
         return p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
     }
 }
