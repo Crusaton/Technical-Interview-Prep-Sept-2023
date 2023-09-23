1794e6c3a2b502aa7f2e35552e594ed4fae519d8
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-23 18:27:59 -0400

    Time: 70 ms (32.05%), Space: 37.8 MB (99.04%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cs
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cs
@@ -6,27 +6,37 @@ public class Solution {
         int skipT = 0;
         while(i >= 0 || j >= 0)
         {
+            // While we haven't hit the end of the string S.
             while(i >= 0)
             {
+                // If we hit a backspace character
                 if(s[i] == '#')
                 {
+                    // We increment the skip counter and decrease the iterator I
                     skipS++; i--;
                 }
+                // Else if we didn't hit a backsapce character but the character before it is
                 else if(skipS > 0)
                 {
+                    // We decrease both iterators
                     skipS--; i--;
                 }
                 else
                 {
+                    // We haven't hit any backspace character so we break the loop.
                     break;
                 }
             }
+            // While we haven't hit the end of the string T.
             while(j >= 0)
             {
+                // If we found a backspace character 
                 if(t[j] == '#')
                 {
+                    // We increment the counter of what we skip and decrease the iterator J.
                     skipT++; j--;
                 }
+                // If 
                 else if (skipT > 0 )
                 {
                     skipT--; j--;
