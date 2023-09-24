241ee6d7e11e48d8fcf7972b5d6894ab318ede6c
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-24 19:37:49 -0400

    Time: 102 ms (40.46%), Space: 42.8 MB (36.64%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.cs
+++ 0252-meeting-rooms/0252-meeting-rooms.cs
@@ -1,10 +1,14 @@
 public class Solution {
     public bool CanAttendMeetings(int[][] intervals) {
         
+        
+        // Sort the 2D array by the first element of each of the child arrays.
         Array.Sort(intervals, (a, b) => { return a[0] - b[0]; });
         
+        // Iterate through the 2D array.
         for(int i = 0; i < intervals.Length - 1; i++)
         {
+            // If the end time value of the left side is greater than the start time value of the right side we know they can't attend both meetings. 
             if(intervals[i][1] > intervals[i + 1][0])
             {
                 return false;
