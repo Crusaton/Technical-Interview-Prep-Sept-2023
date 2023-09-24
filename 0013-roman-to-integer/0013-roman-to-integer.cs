5c74d6d319a7dd2697e312cfae7835ab927dd386
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-24 19:31:40 -0400

    Time: 65 ms (67.25%), Space: 46.4 MB (92.60%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cs
+++ 0013-roman-to-integer/0013-roman-to-integer.cs
@@ -1,4 +1,6 @@
 public class Solution {
+    
+    // Hashmap/dictionary to have our roman letters and their respective decimal value.
     static Dictionary<char, int> values = new() {
         {'M', 1000},
         {'D', 500},
@@ -9,22 +11,32 @@ public class Solution {
         {'I', 1}
     };
     public int RomanToInt(string s) {
+        // Grab the last symbol of the string.
         char lastSymbol = s[s.Length - 1];
+        // Grab the last symbol's value.
         int lastValue = values[lastSymbol];
+        // Set our total to the last symbol's value.
         int total = lastValue;
         
+        // We are iterating backwards from the end of the string.
         for(int i = s.Length - 2; i >= 0; i--)
         {
+            // Grab the current symbol
             char curr = s[i];
+            // Grab the current value
             int currValue = values[curr];
+            // If our current value is less than the previous value we need to subtract
+            // Similar to how IV works
             if(currValue < lastValue)
             {
                 total -= currValue;
             }
+            // Else if our currvalue is greater we can just add it
             else
             {
                 total += currValue;
             }
+            // Save curr as our previously seeen value.
             lastValue = currValue;
         }
         return total;
