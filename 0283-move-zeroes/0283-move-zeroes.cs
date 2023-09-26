a55ff38d4af3a6ea74f2be5cfbad97d4cdd18b64
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-25 20:46:56 -0400

    Time: 140 ms (87.67%), Space: 54 MB (86.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cs
+++ 0283-move-zeroes/0283-move-zeroes.cs
@@ -1,21 +1,29 @@
 public class Solution {
     public void MoveZeroes(int[] nums) {
         
+        // Two pointer approach
         int readIndex = 0;
         int writeIndex = 0;
         
+        // While we haven't hit the end of the array. (While we are still reading values)
         while (readIndex < nums.Length)
         {
+            // If we encounter a 0
             if(nums[readIndex] == 0)
             {
+                // Increment our read index one and continue
                 readIndex++;
                 continue;
             }
+            // Now that we encountered a 0 our indices don't match
             if(readIndex != writeIndex)
             {
+                // Set the writeIndex value to the postion in front of it.
                 nums[writeIndex] = nums[readIndex];
+                // Move the 0 towards the end of the list.
                 nums[readIndex] = 0;
             }
+            // Increment both our indices
             writeIndex++;
             readIndex++;
         }
