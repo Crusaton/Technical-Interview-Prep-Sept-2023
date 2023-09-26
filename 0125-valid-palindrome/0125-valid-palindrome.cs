d50b5e7f7fa1edd3b738120b3e1ffe5ad82368a8
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-25 20:13:20 -0400

    Time: 63 ms (90.93%), Space: 40.7 MB (67.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cs
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -1,14 +1,23 @@
 public class Solution {
     public bool IsPalindrome(string s) {
-        string cleaned = new string(s.Where(Char.IsLetterOrDigit).ToArray()).ToLower();
         
-        for(int i = 0; i < cleaned.Length; i++)
+        StringBuilder sb = new();
+        foreach(var c in s)
         {
-            if(cleaned[i] != cleaned[cleaned.Length - i - 1])
+            if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0') && c <= '9')
+            {
+                sb.Append(Char.ToLower(c));
+            }
+        }
+        s = sb.ToString();
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(s[i] != s[s.Length - i - 1])
             {
                 return false;
             }
         }
         return true;
+        
     }
 }
