6126025b2bcae7cc6251464b4bc301df6670aa44
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-26 20:33:55 -0400

    Time: 89 ms (74.84%), Space: 43.1 MB (51.94%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cs
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cs
@@ -11,14 +11,15 @@
  */
 public class Solution {
     public bool HasCycle(ListNode head) {
-        ListNode fast = head;
         ListNode slow = head;
+        ListNode fast = head;
         
         while(fast != null && fast.next != null)
         {
-            fast = fast.next.next;
             slow = slow.next;
-            if(fast == slow)
+            fast = fast.next.next;
+            
+            if(slow == fast)
             {
                 return true;
             }
