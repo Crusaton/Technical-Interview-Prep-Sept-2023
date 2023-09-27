9b4876573eb66d771f5bae5d4208859730c832f8
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-09-26 20:39:29 -0400

    Time: 71 ms (91.96%), Space: 39.3 MB (33.57%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cs
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cs
@@ -11,21 +11,13 @@
  */
 public class Solution {
     public ListNode ReverseList(ListNode head) {
-        
-        // Prev is null so we can set our previous head's next value to null.
-        // Curr is our first value so head.
         ListNode prev = null, curr = head;
         
-        // While we haven't reached the end of the original list.
         while(curr != null)
         {
-            // Save the next value from curr.
-            var temp = curr.next;
-            // Point curr's next backwards.
+            ListNode temp = curr.next;
             curr.next = prev;
-            // Move our previous value up one.
             prev = curr;
-            // Move curr up one.
             curr = temp;
         }
         return prev;
