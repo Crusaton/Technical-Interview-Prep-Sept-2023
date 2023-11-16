c23471583a8b3e60a4969ff1875e21bf4e36e469
Paul Rodriguez <46976659+Crusaton@users.noreply.github.com>
2023-11-15 22:36:07 -0500

    Time: 202 ms (91.81%), Space: 49.6 MB (94.57%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
@@ -1,11 +1,13 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
+        
         int min = prices[0];
         int max = 0;
         
         for(int i = 0; i < prices.Length; i++)
         {
-            min = Math.Min(min, prices[i]);
+            int currMin = prices[i];
+            min = Math.Min(min, currMin);
             max = Math.Max(max, prices[i] - min);
         }
         return max;
